cefa2a10b296fec01d86d229453549a0dc2530de
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2023-06-29 18:00:02 +0200

    PrintString

--- print_string.c
+++ print_string.c
@@ -16,32 +16,32 @@
  */
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
 {
-    char *str = va_arg(types, char*);
-    int count = 0;
-    int i = 0;
+char *str = va_arg(types, char*);
+int count = 0;
+int i = 0;
 
-    if (str == NULL)
-        str = "(null)";
 
-    while (str[i] != '\0') {
-        if (str[i] < ' ' || str[i] >= 127) {
-            count += snprintf(buffer + count, BUFFER_SIZE - count, "\x%02X", (unsigned char)str[i]);
-        } else {
-            buffer[count++] = str[i];
-        }
+if (str == NULL)
+str = "(null)";
 
-        if (count >= BUFFER_SIZE - 1) {
-            /* Buffer is full, write it to standard output */
-            write_buffer(buffer, count);
-            count = 0;
-        }
-
-        i++;
-    }
-
-    /* Write remaining characters in the buffer to standard output */
-    write_buffer(buffer, count);
-
-    return (i);
+while (str[i] != '\0')
+{
+if (str[i] < ' ' || str[i] >= 127)
+{
+count += snprintf(buffer + count, BUFFER_SIZE - count, "\x%02X", (unsigned char)str[i]);
 }
-
+else
+{
+buffer[count++] = str[i];
+}
+if (count >= BUFFER_SIZE - 1) {
+/* Buffer is full, write it to standard output */
+write_buffer(buffer, count);
+count = 0;
+}
+i++;
+}
+/* Write remaining characters in the buffer to standard output */
+write_buffer(buffer, count);
+return (i);
+}
