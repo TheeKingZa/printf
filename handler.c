b358db5899c2c54eb34882ac3a150f2d8bf1ee6f
Pule Mathikha <proceedingmc@gmail.com>
2023-06-30 14:06:35 +0200

    Printf Project

--- handler.c
+++ handler.c
@@ -1,48 +1,55 @@
 #include "main.h"
-#include <stdio.h>  /* Include the <stdio.h> header for sprintf */
-#include <string.h> /* Include the <string.h> header for strlen */
 
 /**
- * handler - Handle conversion specifier
- *           based on length modifiers
- * @format: Format specifier string
- * @args: Arguments for the format specifier
- * @buffer: Buffer to store the converted string
- * @count: Pointer to the count of characters printed
+ * handler - Handles the conversion specifiers
+ * @format: The format string
+ * @args: The va_list containing the arguments
+ * @buffer: The buffer to store the formatted string
+ * @count: The count of characters printed
  *
- * Return: The updated count of characters printed
+ * Return: The count of characters printed
  */
 int handler(const char *format, va_list args, char *buffer, int *count)
 {
-/* Implementation code for handling each conversion specifier */
-/* ... */
+	int i = 0, j = 0;
+	char *str;
 
-/* Convert num to string and store it in buffer */
-if (*format == 'd' || *format == 'i')
-{
-int num = va_arg(args, int);
-sprintf(buffer, "%d", num); /* Convert int to string */
-*count += strlen(buffer);   /* Update the count */
-}
-else if (*format == 'u' || *format == 'o' || *format == 'x' || *format == 'X')
-{
-unsigned int num = va_arg(args, unsigned int);
-sprintf(buffer, "%u", num); /* Convert unsigned int to string */
-*count += strlen(buffer);   /* Update the count */
-}
-else if (*format == 'l')
-{
-long num = va_arg(args, long);
-sprintf(buffer, "%ld", num); /* Convert long to string */
-*count += strlen(buffer);    /* Update the count */
-}
-else if (*format == 'h')
-{
-short num = va_arg(args, int); /* Assuming short is promoted to int */
-sprintf(buffer, "%d", num);    /* Convert int to string */
-*count += strlen(buffer);      /* Update the count */
-}
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					buffer[j++] = va_arg(args, int);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str)
+					{
+						buffer[j++] = *str++;
+					}
+					break;
+				case '%':
+					buffer[j++] = '%';
+					break;
+				default:
+					buffer[j++] = '%';
+					buffer[j++] = format[i];
+					break;
+			}
+		}
+		else
+		{
+			buffer[j++] = format[i];
+		}
+		i++;
+		(*count)++;
+	}
 
-return (*count); /* Return the updated count */
+	buffer[j] = '\0';
+
+	return (*count);
 }
 
