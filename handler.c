3aa3433dae0b1c50f3fd4fbac328a3c514c65d43
Pule Mathikha <proceedingmc@gmail.com>
2023-06-30 14:45:18 +0200

    Printf Project

--- handler.c
+++ handler.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * handle_integer - Handles the 'd' and 'i' conversion specifiers
+ * @args: The va_list containing the arguments
+ * @buffer: The buffer to store the output string
+ * @count: A pointer to the count of characters printed
+ *
+ * Return: The number of characters printed
+ */
+int handle_integer(va_list args, char *buffer, int *count)
+{
+	int num = va_arg(args, int);
+	return print_integer(num, buffer + *count);
+}
+
+/**
+ * handle_unsigned_integer - Handles the 'u' conversion specifier
+ * @args: The va_list containing the arguments
+ * @buffer: The buffer to store the output string
+ * @count: A pointer to the count of characters printed
+ *
+ * Return: The number of characters printed
+ */
+int handle_unsigned_integer(va_list args, char *buffer, int *count)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	return print_unsigned_integer(num, buffer + *count);
+}
+
+/* Add similar helper functions for other conversion specifiers */
+
+/**
  * handler - Handles the format specifiers and builds the output string
  * @format: The format string
  * @args: The va_list containing the arguments
@@ -14,67 +44,63 @@ int handler(const char *format, va_list args, char *buffer, int *count)
 {
 	int i, j;
 	char *str;
-	unsigned int num;
 
 	i = 0;
 	j = 0;
 
-while (format[i] != '\0')
-{
-	if (format[i] == '%')
+	while (format[i] != '\0')
 	{
-		i++;
-	switch (format[i])
-	{
-		case 'd':
-		case 'i':
-		num = va_arg(args, int);
-		j += print_integer(num, buffer + j);
-			break;
-		case 'u':
-		num = va_arg(args, unsigned int);
-		j += print_unsigned_integer(num, buffer + j);
-			break;
-		case 'o':
-		num = va_arg(args, unsigned int);
-		j += print_octal(num, buffer + j);
-			break;
-		case 'x':
-		num = va_arg(args, unsigned int);
-		j += print_hexadecimal(num, 0, buffer + j);
-			break;
-		case 'X':
-		num = va_arg(args, unsigned int);
-		j += print_hexadecimal(num, 1, buffer + j);
-			break;
-		case 'c':
-		buffer[j++] = va_arg(args, int);
-			break;
-		case 's':
-		str = va_arg(args, char *);
-		j += print_string(str, buffer + j);
-			break;
-		case 'p':
-		str = va_arg(args, void *);
-		j += print_address(str, buffer + j);
-			break;
-		case '%':
-		buffer[j++] = '%';
-			break;
-		default:
-		buffer[j++] = '%';
-		buffer[j++] = format[i];
-			break;
-		}
+		if (format[i] == '%')
+		{
+			i++;
+
+			switch (format[i])
+			{
+				case 'd':
+				case 'i':
+					j += handle_integer(args, buffer, count);
+					break;
+				case 'u':
+					j += handle_unsigned_integer(args, buffer, count);
+					break;
+				case 'o':
+					/* Handle 'o' conversion */
+					break;
+				case 'x':
+					/* Handle 'x' conversion */
+					break;
+				case 'X':
+					/* Handle 'X' conversion */
+					break;
+				case 'c':
+					/* Handle 'c' conversion */
+					break;
+				case 's':
+					/* Handle 's' conversion */
+					break;
+				case 'p':
+					/* Handle 'p' conversion */
+					break;
+				case '%':
+					buffer[j++] = '%';
+					break;
+				default:
+					buffer[j++] = '%';
+					buffer[j++] = format[i];
+					break;
+			}
 		}
 		else
 		{
 			buffer[j++] = format[i];
 		}
+
 		i++;
 	}
+
 	*count = j;
 	buffer[j] = '\0';
-	return (j);
+
+	return j;
 }
 
