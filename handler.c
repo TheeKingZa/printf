fc157ddff2cd0881e15f1dec4a50de1b9c977cf4
Pule Mathikha <proceedingmc@gmail.com>
2023-06-30 14:40:46 +0200

    Printf Project

--- handler.c
+++ handler.c
@@ -19,66 +19,62 @@ int handler(const char *format, va_list args, char *buffer, int *count)
 	i = 0;
 	j = 0;
 
-	while (format[i] != '\0')
+while (format[i] != '\0')
+{
+	if (format[i] == '%')
 	{
-		if (format[i] == '%')
-		{
-			i++;
-
-			switch (format[i])
-			{
-				case 'd':
-				case 'i':
-					num = va_arg(args, int);
-					j += print_integer(num, buffer + j);
-					break;
-				case 'u':
-					num = va_arg(args, unsigned int);
-					j += print_unsigned_integer(num, buffer + j);
-					break;
-				case 'o':
-					num = va_arg(args, unsigned int);
-					j += print_octal(num, buffer + j);
-					break;
-				case 'x':
-					num = va_arg(args, unsigned int);
-					j += print_hexadecimal(num, 0, buffer + j);
-					break;
-				case 'X':
-					num = va_arg(args, unsigned int);
-					j += print_hexadecimal(num, 1, buffer + j);
-					break;
-				case 'c':
-					buffer[j++] = va_arg(args, int);
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					j += print_string(str, buffer + j);
-					break;
-				case 'p':
-					str = va_arg(args, void *);
-					j += print_address(str, buffer + j);
-					break;
-				case '%':
-					buffer[j++] = '%';
-					break;
-				default:
-					buffer[j++] = '%';
-					buffer[j++] = format[i];
-					break;
-			}
+		i++;
+	switch (format[i])
+	{
+		case 'd':
+		case 'i':
+		num = va_arg(args, int);
+		j += print_integer(num, buffer + j);
+			break;
+		case 'u':
+		num = va_arg(args, unsigned int);
+		j += print_unsigned_integer(num, buffer + j);
+			break;
+		case 'o':
+		num = va_arg(args, unsigned int);
+		j += print_octal(num, buffer + j);
+			break;
+		case 'x':
+		num = va_arg(args, unsigned int);
+		j += print_hexadecimal(num, 0, buffer + j);
+			break;
+		case 'X':
+		num = va_arg(args, unsigned int);
+		j += print_hexadecimal(num, 1, buffer + j);
+			break;
+		case 'c':
+		buffer[j++] = va_arg(args, int);
+			break;
+		case 's':
+		str = va_arg(args, char *);
+		j += print_string(str, buffer + j);
+			break;
+		case 'p':
+		str = va_arg(args, void *);
+		j += print_address(str, buffer + j);
+			break;
+		case '%':
+		buffer[j++] = '%';
+			break;
+		default:
+		buffer[j++] = '%';
+		buffer[j++] = format[i];
+			break;
+		}
 		}
 		else
 		{
 			buffer[j++] = format[i];
 		}
-
 		i++;
 	}
-
 	*count = j;
 	buffer[j] = '\0';
-
 	return (j);
 }
 
