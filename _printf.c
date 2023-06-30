e42f4242ce4a738ab682d25020d8321942119b5e
Pule Mathikha <proceedingmc@gmail.com>
2023-06-30 17:28:31 +0200

    Printf Project

--- _printf.c
+++ _printf.c
@@ -13,77 +13,48 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-va_list args;
-int count = 0;
-char buffer[BUFFER_SIZE];
-char *buffer_ptr = buffer;
+	va_list args;
+	int count = 0;
+	char buffer[BUFFER_SIZE];
+	char *buffer_ptr = buffer;
 
-va_start(args, format);
+	va_start(args, format);
 
-while (*format)
-{
-	if (*format == '%')
+	while (*format)
 	{
-	format++;
-/* Handle conversion specifier */
-	count += handler(format, args, buffer_ptr, &count);
+		if (*format == '%')
+		{
+			format++;
+
+			/* Handle conversion specifier */
+			count += handler(format, args, buffer_ptr, &count);
+		}
+		else
+		{
+			/* Regular character, copy to buffer */
+			*buffer_ptr = *format;
+			buffer_ptr++;
+			count++;
+		}
+
+		format++;
+
+		/* Flush the buffer if it is full */
+		if (buffer_ptr == buffer + BUFFER_SIZE)
+		{
+			write(1, buffer, BUFFER_SIZE);
+			buffer_ptr = buffer;
+		}
 	}
-	else
+
+	/* Write any remaining characters in the buffer */
+	if (buffer_ptr != buffer)
 	{
-/* Regular character, copy to buffer */
-	*buffer_ptr = *format;
-	buffer_ptr++;
-	count++;
+		write(1, buffer, buffer_ptr - buffer);
 	}
-	format++;
-}
-    /* Write the buffer to the standard output */
-    /* write_buffer(buffer, buffer_ptr);*/
-=======
-    va_list args;
-    int count = 0;
-    char buffer[BUFFER_SIZE];
-    char *buffer_ptr = buffer;
 
-    va_start(args, format);
+	va_end(args);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            /* Handle conversion specifier */
-            count += handler(format, args, buffer_ptr, &count);
-        }
-        else
-        {
-            /* Regular character, copy to buffer */
-            *buffer_ptr = *format;
-            buffer_ptr++;
-            count++;
-        }
-
-        format++;
-
-        /* Flush the buffer if it is full */
-        if (buffer_ptr == buffer + BUFFER_SIZE)
-        {
-            write(1, buffer, BUFFER_SIZE);
-            buffer_ptr = buffer;
-        }
-    }
-
-    /* Write any remaining characters in the buffer */
-    if (buffer_ptr != buffer)
-    {
-        write(1, buffer, buffer_ptr - buffer);
-    }
->>>>>>> eb29c9f1c50089c01e50b76a6f2259e5cf71a495
-
-va_end(args);
-
-return (count);
+	return (count);
 }
 
