fda7fa3fef6e86c136b8ab04efdc7a2164544b74
Pule <proceedingmc@gmail.com>
2023-07-08 15:44:19 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,81 +1,64 @@
 #include <stdarg.h>
-#include <unistd.h>
-#include "main.h"
+#include <stdio.h>
+
 /**
- * _printf - Custom printf function
- * @format: Format string containing conversion specifiers
+ * _printf - Prints formatted output to stdout and returns
+ *      the number of characters printed
+ *      (excluding the null byte)
+ * @format: A pointer to a constant character string
+ *              representing the format specifier
+ * @...: Additional arguments corresponding to the values referenced
+ *              by the placeholders in the format string
  *
- * Return: The number of characters printed (excluding the null byte).
+ * Return: The number of characters printed (excluding the null byte)
+ *
+ * Description: The format string is composed of
+ *              zero or more directives.
+ * The following conversion specifiers are handled:
+ * - %c: Prints a single character.
+ * - %s: Prints a null-terminated string.
+ * - %%: Prints a single percent character.
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i = 0, count = 0;
-    va_list args;
-    va_start(args, format);
-    if (!format)
-        return (-1);
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                _putchar(va_arg(args, int));
-                count++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                count += print_string(va_arg(args, char *));
-            }
-            else if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else if (format[i + 1] == 'i' || format[i + 1] == 'd')
-            {
-                count += print_int(va_arg(args, int));
-            }
-            else if (format[i + 1] == 'b')
-            {
-                count += print_binary(va_arg(args, int));
-            }
-            /*
-                       else if (format[i + 1] == 'u')
-            {
-                count += print_unsigned(va_arg(args, unsigned int));
-            }
-            else if (format[i + 1] == 'o')
-            {
-                count += print_octal(va_arg(args, unsigned int));
-            }
-            else if (format[i + 1] == 'x')
-            {
-                count += print_hex_low(va_arg(args, unsigned int));
-            }
-            else if (format[i + 1] == 'X')
-            {
-                count += print_hex_upp(va_arg(args, unsigned int));
-            }
-            else if (format[i + 1] == 'S')
-            {
-                count += print_non(va_arg(args, char *));
-            }
-            */
-            else
-            {
-                _putchar(format[i]);
-                count++;
-            }
-            i++;
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+va_list args;
+va_start(args, format);
+int count = 0;
+/* Initialize a count variable number of characters printed */
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+/* Move past the '%' */
+if (
+*format == 'c')
+{
+int c = va_arg(args, int);
+/* Fetch the next argument as an int(byte(4) of int) */
+count += _putchar(c);
+/* Print the character and increment the count */
+}
+else if (*format == 's')
+{
+char *s = va_arg(args, char *);
+/* Fetch the next argument as a char pointer */
+count += printf("%s", s);/* Print the string and increment the count */
+}
+else if (*format == '%')
+{
+count += _putchar('%');/* Print a percent character and increment the count */
+}
+}
+else
+{
+count += _putchar(*format);
+/* Print a regular character and increment the count */
+}
+format++;
+/* Move to the next character in the format string */
+}
+va_end(args);
+return (count);
 }
