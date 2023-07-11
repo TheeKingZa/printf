ba05df1c660c5aff7b790aafe42e15dfbac92782
Pule <proceedingmc@gmail.com>
2023-07-11 03:24:53 -0700

    Cleaning

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+/* Prototypes */
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_integer(int num);
+int print_binary(unsigned int num);
+
+
+#endif
+
